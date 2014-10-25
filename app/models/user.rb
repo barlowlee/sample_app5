cf3a7fc0a20a722eaebbec53e46c2587cedb0366
Jonathan Lee <jonathanbarlowlee@gmail.com>
2014-10-24 18:37:14 -0700

    Set up user-micropost associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+	has_many :microposts
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 		# this is a private method below, in user.rb
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Micropost do
+
+	let(:user) { FactoryGirl.create(:user) }
+	before do
+		# This code is not idiomatically correct.
+		# @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+
+		# However, this code is correct:
+		@micropost = user.microposts.build(content: "Lorem ipsum")
+	end
+
+	subject { @micropost }
+
+	it { should respond_to(:content) }
+	it { should respond_to(:user_id) }
+	it { should respond_to(:user) }
+	its(:user) { should eq user }
+	it { should be_valid }
+
+	describe "- when user_id is not present" do
+		before { @micropost.user_id = nil }
+		it { should_not be_valid }
+	end
+end
